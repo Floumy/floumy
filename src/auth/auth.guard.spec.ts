ce97d30668a4851fd98e9f28d08e1e2f1fcbe48f
Alexandru Nedelcu <lexnede@gmail.com>
2023-10-31 21:42:14 +0100

    Add test for auth guard

---
+++ src/auth/auth.guard.spec.ts
@@ -0,0 +1,146 @@
+import {AuthService} from './auth.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {UsersModule} from '../users/users.module';
+import {jwtModule} from '../../test/jwt.test-module';
+import {AuthGuard} from './auth.guard';
+import {JwtService} from '@nestjs/jwt';
+import {Reflector} from '@nestjs/core';
+import {ExecutionContext} from '@nestjs/common';
+
+describe("AuthGuard", () => {
+    let jwtService: JwtService;
+    let module: TestingModule;
+
+    beforeEach(async () => {
+        module = await Test.createTestingModule({
+            imports: [
+                UsersModule,
+                jwtModule
+            ],
+            providers: [AuthService]
+        }).compile();
+
+        jwtService = module.get<JwtService>(JwtService);
+    });
+
+    it("should be defined", () => {
+        const reflector = module.get<Reflector>(Reflector);
+        const guard = new AuthGuard(jwtService, reflector);
+        expect(guard).toBeInstanceOf(AuthGuard);
+    });
+
+    it("should return true when the handler is public", async () => {
+        // TODO: Refactor this horrendous mock
+        const reflector = {
+            getAllAndOverride: () => {
+                return true;
+            }
+        } as unknown as Reflector;
+        const guard = new AuthGuard(jwtService, reflector);
+        const context = {
+            getHandler: () => {
+            },
+            getClass: () => {
+            }
+        } as ExecutionContext;
+        const isPublic = await guard["isPublic"](context);
+        expect(isPublic).toBe(true);
+    });
+
+    it("should return false when the handler is not public", async () => {
+        // TODO: Refactor this horrendous mock
+        const reflector = {
+            getAllAndOverride: () => {
+                return false;
+            }
+        } as unknown as Reflector;
+        const guard = new AuthGuard(jwtService, reflector);
+        const context = {
+            getHandler: () => {
+            },
+            getClass: () => {
+            }
+        } as ExecutionContext;
+        const isPublic = await guard["isPublic"](context);
+        expect(isPublic).toBe(false);
+    });
+
+    it("should throw an error when no token is provided", async () => {
+        // TODO: Refactor this horrendous mock
+        const reflector = {
+            getAllAndOverride: () => {
+                return false;
+            }
+        } as unknown as Reflector;
+        const guard = new AuthGuard(jwtService, reflector);
+        const context = {
+            switchToHttp: () => {
+                return {
+                    getRequest: () => {
+                        return {
+                            headers: {
+                                authorization: undefined
+                            }
+                        }
+                    }
+                }
+            }
+        } as ExecutionContext;
+        await expect(guard.canActivate(context)).rejects.toThrow();
+    });
+
+    it("should throw an error when the token is invalid", async () => {
+        // TODO: Refactor this horrendous mock
+        const reflector = {
+            getAllAndOverride: () => {
+                return false;
+            }
+        } as unknown as Reflector;
+        const guard = new AuthGuard(jwtService, reflector);
+        const context = {
+            switchToHttp: () => {
+                return {
+                    getRequest: () => {
+                        return {
+                            headers: {
+                                authorization: "Bearer invalid"
+                            }
+                        }
+                    }
+                }
+            }
+        } as ExecutionContext;
+        await expect(guard.canActivate(context)).rejects.toThrow();
+    });
+
+    it("should return true when the token is valid", async () => {
+        // TODO: Refactor this horrendous mock
+        const reflector = {
+            getAllAndOverride: () => {
+                return false;
+            }
+        } as unknown as Reflector;
+        const guard = new AuthGuard(jwtService, reflector);
+        const context = {
+            getHandler: () => {
+
+            },
+            getClass: () => {
+
+            },
+            switchToHttp: () => {
+                return {
+                    getRequest: () => {
+                        return {
+                            headers: {
+                                authorization: "Bearer " + jwtService.sign({sub: "test", username: "test"})
+                            }
+                        }
+                    }
+                }
+            }
+        } as ExecutionContext;
+        const canActivate = await guard.canActivate(context);
+        expect(canActivate).toBe(true);
+    });
+});
