0026399c83bd23951f3863fefdcd25dd6857e086
Alexandru Nedelcu <lexnede@gmail.com>
2023-11-06 08:31:30 +0100

    Add ability to create Orgs

--- src/orgs/orgs.service.spec.ts
+++ src/orgs/orgs.service.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { OrgsService } from "./orgs.service";
+import { Org } from "./org.entity";
+import { User } from "../users/user.entity";
+import { UsersService } from "../users/users.service";
+import { typeOrmModule } from "../../test/typeorm.test-module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 
 describe("OrgsService", () => {
   let service: OrgsService;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrgsService]
+      imports: [
+        typeOrmModule,
+        TypeOrmModule.forFeature([User, Org]),
+        ConfigModule
+      ],
+      providers: [OrgsService, UsersService]
     }).compile();
 
     service = module.get<OrgsService>(OrgsService);
+    usersService = module.get<UsersService>(UsersService);
+  });
+
+  afterEach(async () => {
+    await service.clear();
+    await usersService.clear();
   });
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  it("should create an org", async () => {
+    const user = new User("Test User", "test@example.com", "testtesttest");
+    const org = await service.createForUser(user);
+    expect(org).toBeInstanceOf(Org);
+    expect(org.id).toBeDefined();
   });
 });
--- src/orgs/orgs.service.ts
+++ src/orgs/orgs.service.ts
@@ -1,5 +1,22 @@
 import { Injectable } from "@nestjs/common";
+import { User } from "../users/user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Org } from "./org.entity";
 
 @Injectable()
 export class OrgsService {
+
+  constructor(@InjectRepository(Org) private orgRepository: Repository<Org>) {
+  }
+
+  async createForUser(user: User) {
+    const org = new Org();
+    org.users = [user];
+    return this.orgRepository.save(org);
+  }
+
+  async clear() {
+    await this.orgRepository.clear();
+  }
 }
