4efc16df1b9f193432c8619756a833cb6c938e8a
Alexandru Nedelcu <lexnede@gmail.com>
2023-11-07 21:02:32 +0100

    Add objectives

--- src/okrs/okrs.module.ts
+++ src/okrs/okrs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { OkrsService } from "./okrs.service";
 import { OkrsController } from "./okrs.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Objective } from "./objective.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Objective])],
   providers: [OkrsService],
   controllers: [OkrsController]
 })
--- src/orgs/org.entity.ts
+++ src/orgs/org.entity.ts
@@ -1,5 +1,6 @@
 import { CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { User } from "../users/user.entity";
+import { Objective } from "../okrs/objective.entity";
 
 @Entity()
 export class Org {
@@ -11,4 +12,6 @@ export class Org {
   createdAt: Date;
   @UpdateDateColumn()
   updatedAt: Date;
+  @OneToMany(() => Objective, objective => objective.org)
+  objectives: Promise<Objective[]>;
 }
