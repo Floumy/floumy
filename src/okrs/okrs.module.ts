78aff1b17a471680eed5a4ca725e9d1a479d7a28
Alexandru Nedelcu <lexnede@gmail.com>
2023-11-10 21:29:26 +0100

    Add Objectives lists

--- src/okrs/okrs.module.ts
+++ src/okrs/okrs.module.ts
@@ -3,9 +3,11 @@ import { OkrsService } from "./okrs.service";
 import { OkrsController } from "./okrs.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Objective } from "./objective.entity";
+import { OrgsModule } from "../orgs/orgs.module";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Objective])],
+  imports: [TypeOrmModule.forFeature([Objective]), OrgsModule, AuthModule],
   providers: [OkrsService],
   controllers: [OkrsController]
 })
