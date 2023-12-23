8e04a14bed73f23fd30442a36338149127b504f9
Alexandru Nedelcu <lexnede@gmail.com>
2023-12-23 21:31:54 +0200

    Fix the work item mapper

--- src/iterations/iteration.mapper.ts
+++ src/iterations/iteration.mapper.ts
@@ -1,8 +1,26 @@
 import { Iteration } from "./Iteration.entity";
 import { TimelineService } from "../common/timeline.service";
+import { WorkItem } from "../backlog/work-items/work-item.entity";
+
+class WorkItemMapper {
+  static toDto(workItem: WorkItem) {
+    return {
+      id: workItem.id,
+      title: workItem.title,
+      description: workItem.description,
+      priority: workItem.priority,
+      type: workItem.type,
+      status: workItem.status,
+      estimation: workItem.estimation,
+      createdAt: workItem.createdAt,
+      updatedAt: workItem.updatedAt
+    };
+  }
+}
 
 export class IterationMapper {
-  static toDto(iteration: Iteration) {
+  static async toDto(iteration: Iteration) {
+    const workItems = await iteration.workItems;
     return {
       id: iteration.id,
       title: iteration.title,
@@ -10,6 +28,7 @@ export class IterationMapper {
       startDate: iteration.startDate.toISOString().split("T")[0],
       endDate: iteration.endDate.toISOString().split("T")[0],
       timeline: TimelineService.convertDateToTimeline(iteration.startDate),
+      workItems: workItems.map(WorkItemMapper.toDto),
       duration: iteration.duration,
       createdAt: iteration.createdAt,
       updatedAt: iteration.updatedAt
