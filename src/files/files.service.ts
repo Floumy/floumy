d82c81603013ba2bd9309793536e33335d18ccda
Alexandru Nedelcu <lexnede@gmail.com>
2024-01-24 07:37:34 +0100

    Add first version of the upload module

---
+++ src/files/files.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+@Injectable()
+export class FilesService {
+  async uploadFile(filename: string, file: Express.Multer.File) {
+    const s3Client = new S3Client({
+      endpoint: "https://fra1.digitaloceanspaces.com",
+      forcePathStyle: false,
+      region: "fra1", // Must be "us-east-1" when creating new Spaces. Otherwise, use the region in your endpoint (for example, nyc3).
+      credentials: {
+        accessKeyId: process.env.SPACES_KEY,
+        secretAccessKey: process.env.SPACES_SECRET
+      }
+    });
+    await s3Client.send(new PutObjectCommand({
+      Bucket: "work-items-attachments",
+      Key: filename,
+      Body: file.buffer,
+      ACL: "public-read"
+    }));
+  }
+}
