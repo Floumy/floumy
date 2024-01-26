12ca21a5ac9cfea4ac9a5526163301bc7ea84fc2
Alexandru Nedelcu <lexnede@gmail.com>
2024-01-26 21:14:35 +0100

    Fix the environment variables for uploading files to spaces + add error state to the file upload component

--- src/files/files-storage.repository.ts
+++ src/files/files-storage.repository.ts
@@ -1,14 +1,16 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class FilesStorageRepository {
-  constructor(@Inject("S3_CLIENT") private readonly s3Client: S3Client) {
+  constructor(private configService: ConfigService,
+              @Inject("S3_CLIENT") private readonly s3Client: S3Client) {
   }
 
   async storeObject(key: string, fileBuffer: Buffer) {
     await this.s3Client.send(new PutObjectCommand({
-      Bucket: "work-items-attachments",
+      Bucket: this.configService.get("fileStorage.bucket"),
       Key: key,
       Body: fileBuffer,
       ACL: "public-read"
