65cfced86da1fe39813f317b035cc6b91395642b
Alexandru Nedelcu <lexnede@gmail.com>
2024-01-27 08:33:34 +0100

    Add download file functionality

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,9 +1,14 @@
 import {
   Controller,
+  Get,
+  Header,
+  HttpStatus,
   MaxFileSizeValidator,
+  Param,
   ParseFilePipe,
   Post,
   Request,
+  Response,
   UploadedFile,
   UseGuards,
   UseInterceptors
@@ -11,6 +16,7 @@ import {
 import { FileInterceptor } from "@nestjs/platform-express";
 import { FilesService } from "./files.service";
 import { AuthGuard } from "../auth/auth.guard";
+import { Readable } from "stream";
 
 @Controller("files")
 @UseGuards(AuthGuard)
@@ -28,5 +34,24 @@ export class FilesController {
                      })) file: Express.Multer.File,
                    @Request() request) {
     return await this.filesService.uploadFile(request.user.org, file);
+  }
+
+  @Get("/:fileId")
+  @Header("Access-Control-Expose-Headers", "Content-Disposition")
+  async getFile(@Param("fileId") fileId: string, @Request() request, @Response() response) {
+    try {
+      const file = await this.filesService.getFile(request.user.org, fileId);
+      response.setHeader("Content-Type", file.object.ContentType);
+      response.setHeader("Content-Disposition", `attachment; filename="${file.name}"`);
+      if (file.object.Body instanceof Buffer) {
+        response.end(file.object.Body);
+      } else if (file.object.Body instanceof Readable) {
+        file.object.Body.pipe(response);
+      } else {
+        response.status(HttpStatus.INTERNAL_SERVER_ERROR).send();
+      }
+    } catch (e) {
+      response.status(HttpStatus.INTERNAL_SERVER_ERROR).send();
+    }
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -18,16 +18,24 @@ export class FilesService {
 
   async uploadFile(orgId: string, file: Express.Multer.File) {
     const org = await this.orgsRepository.findOneByOrFail({ id: orgId });
-    const objectKey = `${orgId}/${uuidV4()}-${file.originalname}`;
-    await this.filesStorageRepository.storeObject(objectKey, file.buffer);
+    const filePath = `${orgId}/${uuidV4()}-${file.originalname}`;
+    await this.filesStorageRepository.storeObject(filePath, file.buffer);
     const fileEntity = new File();
     fileEntity.org = Promise.resolve(org);
     fileEntity.name = file.originalname;
-    fileEntity.path = objectKey;
+    fileEntity.path = filePath;
     fileEntity.size = file.size;
     fileEntity.type = file.mimetype;
-    fileEntity.url = `${this.configService.get("fileStorage.bucketEndpoint")}/${objectKey}`;
+    fileEntity.url = `${this.configService.get("fileStorage.bucketEndpoint")}/${filePath}`;
     const savedFile = await this.filesRepository.save(fileEntity);
     return { id: savedFile.id, name: savedFile.name, size: savedFile.size, type: savedFile.type };
+  }
+
+  async getFile(orgId: string, fileId: string) {
+    const file = await this.filesRepository.findOneByOrFail({ id: fileId, org: { id: orgId } });
+    return {
+      ...file,
+      object: await this.filesStorageRepository.getObject(file.path)
+    };
   }
 }
