c97a0a6adce7e87841359ca9831c70cb93656f17
Alexandru Nedelcu <lexnede@gmail.com>
2024-03-11 20:31:57 +0100

    Return features for the milestones

--- src/roadmap/milestones/milestones.service.ts
+++ src/roadmap/milestones/milestones.service.ts
@@ -1,21 +1,23 @@
-import { Injectable } from "@nestjs/common";
-import { CreateUpdateMilestoneDto } from "./dtos";
-import { Milestone } from "./milestone.entity";
-import { Repository } from "typeorm";
-import { InjectRepository } from "@nestjs/typeorm";
-import { OrgsService } from "../../orgs/orgs.service";
-import { MilestoneMapper } from "./milestone.mapper";
+import { Injectable } from '@nestjs/common';
+import { CreateUpdateMilestoneDto } from './dtos';
+import { Milestone } from './milestone.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { OrgsService } from '../../orgs/orgs.service';
+import { MilestoneMapper } from './milestone.mapper';
 
 @Injectable()
 export class MilestonesService {
-
   constructor(
-    @InjectRepository(Milestone) private milestoneRepository: Repository<Milestone>,
-    private orgsService: OrgsService
-  ) {
-  }
+    @InjectRepository(Milestone)
+    private milestoneRepository: Repository<Milestone>,
+    private orgsService: OrgsService,
+  ) {}
 
-  async createMilestone(orgId: string, createMilestoneDto: CreateUpdateMilestoneDto) {
+  async createMilestone(
+    orgId: string,
+    createMilestoneDto: CreateUpdateMilestoneDto,
+  ) {
     this.validateMilestone(createMilestoneDto);
     const org = await this.orgsService.findOneById(orgId);
     const milestone = new Milestone();
@@ -24,47 +26,75 @@ export class MilestonesService {
     milestone.dueDate = new Date(createMilestoneDto.dueDate);
     milestone.org = Promise.resolve(org);
     const savedMilestone = await this.milestoneRepository.save(milestone);
-    return MilestoneMapper.toDto(savedMilestone);
+    return await MilestoneMapper.toDto(savedMilestone);
   }
 
   private validateMilestone(createMilestoneDto: CreateUpdateMilestoneDto) {
-    if (!createMilestoneDto.title) throw new Error("Milestone title is required");
-    if (!createMilestoneDto.dueDate) throw new Error("Milestone due date is required");
-    if (!createMilestoneDto.dueDate.match(/^\d{4}-\d{2}-\d{2}$/)) throw new Error("Invalid due date");
+    if (!createMilestoneDto.title)
+      throw new Error('Milestone title is required');
+    if (!createMilestoneDto.dueDate)
+      throw new Error('Milestone due date is required');
+    if (!createMilestoneDto.dueDate.match(/^\d{4}-\d{2}-\d{2}$/))
+      throw new Error('Invalid due date');
   }
 
   async findOneById(orgId: string, id: string) {
-    return await this.milestoneRepository.findOneByOrFail({ org: { id: orgId }, id: id });
+    return await this.milestoneRepository.findOneByOrFail({
+      org: { id: orgId },
+      id: id,
+    });
   }
 
   async listMilestones(orgId: string) {
-    return MilestoneMapper.toListDto(await this.milestoneRepository.find({ where: { org: { id: orgId } }, order: { dueDate: "ASC" } }));
+    return MilestoneMapper.toListDto(
+      await this.milestoneRepository.find({
+        where: { org: { id: orgId } },
+        order: { dueDate: 'ASC' },
+      }),
+    );
   }
 
   async listMilestonesWithFeatures(orgId: string) {
-    const milestones = await this.milestoneRepository.find({ where: { org: { id: orgId } }, order: { dueDate: "ASC" }, relations: ["features"] });
+    const milestones = await this.milestoneRepository.find({
+      where: { org: { id: orgId } },
+      order: { dueDate: 'ASC' },
+      relations: ['features'],
+    });
     return await MilestoneMapper.toListWithFeaturesDto(milestones);
   }
 
   async get(orgId: string, id: string) {
-    const milestone = await this.milestoneRepository.findOneByOrFail({ org: { id: orgId }, id: id });
-    return MilestoneMapper.toDto(milestone);
+    const milestone = await this.milestoneRepository.findOneByOrFail({
+      org: { id: orgId },
+      id: id,
+    });
+    return await MilestoneMapper.toDto(milestone);
   }
 
-  async update(orgId: string, id: string, updateMilestoneDto: CreateUpdateMilestoneDto) {
+  async update(
+    orgId: string,
+    id: string,
+    updateMilestoneDto: CreateUpdateMilestoneDto,
+  ) {
     this.validateMilestone(updateMilestoneDto);
-    const milestone = await this.milestoneRepository.findOneByOrFail({ org: { id: orgId }, id: id });
+    const milestone = await this.milestoneRepository.findOneByOrFail({
+      org: { id: orgId },
+      id: id,
+    });
     milestone.title = updateMilestoneDto.title;
     milestone.description = updateMilestoneDto.description;
     milestone.dueDate = new Date(updateMilestoneDto.dueDate);
     const savedMilestone = await this.milestoneRepository.save(milestone);
-    return MilestoneMapper.toDto(savedMilestone);
+    return await MilestoneMapper.toDto(savedMilestone);
   }
 
   async delete(orgId: string, id: string) {
-    const milestone = await this.milestoneRepository.findOneByOrFail({ org: { id: orgId }, id: id });
+    const milestone = await this.milestoneRepository.findOneByOrFail({
+      org: { id: orgId },
+      id: id,
+    });
     const features = await milestone.features;
-    features.forEach(feature => feature.milestone = null);
+    features.forEach((feature) => (feature.milestone = null));
     await this.milestoneRepository.manager.save(features);
     await this.milestoneRepository.remove(milestone);
   }
