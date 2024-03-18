a5af568f8c42f5b6aacf77bce3ff3de7045b9e01
Alexandru Nedelcu <lexnede@gmail.com>
2024-03-18 21:42:48 +0100

    Add fastify for speeding up the requests

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
-import "dotenv/config";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import * as bodyParser from "body-parser";
+import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ["error", "warn"]
-  });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: {
+        level: 'warn',
+      },
+    }),
+  );
   app.enableCors({
     origin: process.env.FRONTEND_URL,
-    credentials: true
+    credentials: true,
   });
-  app.use(bodyParser.json({ limit: "300mb" }));
-  app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-  await app.listen(8080);
+  app.use(bodyParser.json({ limit: '300mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  await app.listen(8080, '0.0.0.0');
 }
 
 bootstrap();
