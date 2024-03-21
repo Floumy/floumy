844a402881b1a7236a8c12394e63a791fe8ddd6e
Alexandru Nedelcu <lexnede@gmail.com>
2024-03-21 21:33:04 +0100

    Fix code smells and bugs found by Sonarqube

--- test/typeorm.test-module.ts
+++ test/typeorm.test-module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import databaseConfig from '../src/config/database.config';
 import { testDbOptions } from './test-db.options';
 
-// TODO: Fix flaky tests
 const typeOrmModule = TypeOrmModule.forRootAsync({
   imports: [
     ConfigModule.forRoot({
