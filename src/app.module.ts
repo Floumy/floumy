56366967af04e120f47afc21ca8c8e84d41da792
Alexandru Nedelcu <lexnede@gmail.com>
2024-04-07 20:58:43 +0300

    Fix deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -64,7 +63,6 @@ import { CacheModule } from '@nestjs/cache-manager';
     FilesModule,
     NotificationsModule,
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
