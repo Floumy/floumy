3e4449cc835498031d25971598d556fb10ad8720
Alexandru Nedelcu <lexnede@gmail.com>
2024-06-04 21:44:06 +0200

    Attempt to fix the failing tests + fix the stripe functionality

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -9,13 +9,13 @@ export class PaymentsController {
 
   @Public()
   @Post('/webhook')
-  async webhook(@Request() req: any) {
-    const sig = req.headers['stripe-signature'];
+  async webhook(@Request() request: any) {
+    const sig = request.headers['stripe-signature'];
 
     let event: Stripe.Event;
 
     try {
-      event = this.paymentService.constructEvent(sig, req.body);
+      event = this.paymentService.constructEvent(sig, request.body);
     } catch (err) {
       return { error: `Webhook Error: ${err.message}` };
     }
@@ -26,23 +26,20 @@ export class PaymentsController {
   }
 
   @Post('/checkout-session')
-  async createCheckoutSession(@Request() req: any) {
-    const { customerId, priceId } = req.body;
+  async createCheckoutSession(@Request() request: any) {
+    const org = request.user.org;
 
-    const sessionUrl = await this.paymentService.createCheckoutSessionUrl(
-      customerId,
-      priceId,
-    );
+    const sessionUrl = await this.paymentService.createCheckoutSessionUrl(org);
 
     return { url: sessionUrl };
   }
 
   @Get('/has-active-subscriptions')
-  async hasActiveSubscription(@Request() req: any) {
-    const { customerId } = req.body;
+  async hasActiveSubscription(@Request() request: any) {
+    const org = request.user.org;
 
     const hasActiveSubscription =
-      await this.paymentService.hasActiveSubscriptions(customerId);
+      await this.paymentService.hasActiveSubscriptions(org);
 
     return { hasActiveSubscription };
   }
