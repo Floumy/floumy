0c3bd52a51f6c1f36b74ae67d16a76b49db7cf0b
Alexandru Nedelcu <lexnede@gmail.com>
2024-06-13 22:41:21 +0200

    Fix if the trial has expired, users can't sign in issue

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,14 +11,15 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from '../auth/auth.guard';
+import { BasicAuthGuard } from '../auth/basic-auth.guard';
 
 @Controller('users')
-@UseGuards(AuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('me')
   @HttpCode(HttpStatus.OK)
+  @UseGuards(BasicAuthGuard)
   async getCurrentUser(@Request() req: any) {
     try {
       return await this.usersService.findOne(req.user.sub);
@@ -29,6 +30,7 @@ export class UsersController {
 
   @Post(':id/deactivate')
   @HttpCode(HttpStatus.ACCEPTED)
+  @UseGuards(AuthGuard)
   async deactivateUser(@Request() req: any, @Param('id') id: string) {
     try {
       await this.usersService.deactivate(req.user.org, id);
