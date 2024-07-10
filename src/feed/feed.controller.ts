84ac0ca48dff48b24ac9d7fa90121367f7868d89
Alexandru Nedelcu <lexnede@gmail.com>
2024-07-10 21:25:43 +0200

    Add feed items listing functionality

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { FeedService } from './feed.service';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('feed')
-export class FeedController {}
+@UseGuards(AuthGuard)
+export class FeedController {
+  constructor(private readonly feedService: FeedService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async listFeedItems(
+    @Request() request,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 0,
+  ) {
+    try {
+      return await this.feedService.listFeedItems(
+        request.user.org,
+        page,
+        limit,
+      );
+    } catch (e) {
+      throw new BadRequestException(e.message);
+    }
+  }
+}
--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FeedItem } from './feed-item.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class FeedService {}
+export class FeedService {
+  constructor(
+    @InjectRepository(FeedItem)
+    private readonly feedItemRepository: Repository<FeedItem>,
+  ) {}
+
+  async listFeedItems(orgId: string, page: number, limit: number) {
+    return this.feedItemRepository.find({
+      where: { org: { id: orgId } },
+      take: limit,
+      skip: (page - 1) * limit,
+      order: { createdAt: 'DESC' },
+    });
+  }
+}
