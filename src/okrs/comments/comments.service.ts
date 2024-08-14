0d8dbe56ad40d9a3f100a9322e15b224a53143f3
Alexandru Nedelcu <lexnede@gmail.com>
2024-08-14 21:11:24 +0200

    Add okrs comments service

--- src/okrs/comments/comments.service.ts
+++ src/okrs/comments/comments.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { KeyResultComment } from '../key-result-comment.entity';
 import { User } from '../../users/user.entity';
+import { CommentMapper } from '../../comments/mappers';
 
 @Injectable()
 export class CommentsService {
@@ -40,7 +41,42 @@ export class CommentsService {
     comment.content = content;
     comment.keyResult = Promise.resolve(keyResult);
     comment.createdBy = Promise.resolve(user);
+    comment.org = Promise.resolve(org);
     await this.keyResultCommentRepository.save(comment);
     return comment;
+  }
+
+  async updateComment(userId: string, commentId: string, content: string) {
+    const comment = await this.keyResultCommentRepository.findOneByOrFail({
+      id: commentId,
+      createdBy: { id: userId },
+    });
+
+    const org = await comment.org;
+
+    if (org?.paymentPlan !== PaymentPlan.PREMIUM) {
+      throw new Error('You need to upgrade to premium to update comments');
+    }
+
+    comment.content = content;
+
+    await this.keyResultCommentRepository.save(comment);
+
+    return CommentMapper.toDto(comment);
+  }
+
+  async deleteComment(userId: string, commentInd: string) {
+    const comment = await this.keyResultCommentRepository.findOneByOrFail({
+      id: commentInd,
+      createdBy: { id: userId },
+    });
+
+    const org = await comment.org;
+
+    if (org?.paymentPlan !== PaymentPlan.PREMIUM) {
+      throw new Error('You need to upgrade to premium to delete comments');
+    }
+
+    await this.keyResultCommentRepository.remove(comment);
   }
 }
