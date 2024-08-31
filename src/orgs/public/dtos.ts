7e56605ca51ef59e1ab2c53ecc1d97f0d5d2cb3b
Alexandru Nedelcu <lexnede@gmail.com>
2024-08-31 21:29:52 +0300

    Feature requests available only for premium orgs

--- src/orgs/public/dtos.ts
+++ src/orgs/public/dtos.ts
@@ -1,6 +1,9 @@
+import { PaymentPlan } from '../../auth/payment.plan';
+
 export interface PublicOrgDto {
   id: string;
   name: string;
+  paymentPlan: PaymentPlan;
   createdAt: Date;
   updatedAt: Date;
 }
