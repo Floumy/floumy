87fa9e65a9687f7da802c58cf48e2e59e38708a0
Alexandru Nedelcu <lexnede@gmail.com>
2024-09-12 22:01:58 +0300

    Issues service endpoints

--- src/issues/issues.controller.spec.ts
+++ src/issues/issues.controller.spec.ts
@@ -1,18 +1,166 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { IssuesController } from './issues.controller';
+import { Org } from '../orgs/org.entity';
+import { User } from '../users/user.entity';
+import { Repository } from 'typeorm';
+import { setupTestingModule } from '../../test/test.utils';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from '../users/users.module';
+import { OrgsService } from '../orgs/orgs.service';
+import { UsersService } from '../users/users.service';
+import { IssuesService } from './issues.service';
+import { PaymentPlan } from '../auth/payment.plan';
+import { Issue } from './issue.entity';
 
 describe('IssuesController', () => {
   let controller: IssuesController;
+  let cleanup: () => Promise<void>;
+  let org: Org;
+  let user: User;
+  let orgsRepository: Repository<Org>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [IssuesController],
-    }).compile();
-
+    const { module, cleanup: dbCleanup } = await setupTestingModule(
+      [TypeOrmModule.forFeature([Org, User, Issue]), UsersModule],
+      [IssuesService],
+      [IssuesController],
+    );
+    cleanup = dbCleanup;
     controller = module.get<IssuesController>(IssuesController);
+    const orgsService = module.get<OrgsService>(OrgsService);
+    const usersService = module.get<UsersService>(UsersService);
+    user = await usersService.createUserWithOrg(
+      'Test User',
+      'test@example.com',
+      'testtesttest',
+    );
+    org = await orgsService.createForUser(user);
+    orgsRepository = module.get<Repository<Org>>(getRepositoryToken(Org));
+    org.paymentPlan = PaymentPlan.PREMIUM;
+    await orgsRepository.save(org);
+  });
+
+  afterEach(async () => {
+    await cleanup();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('when adding a new issue', () => {
+    it('should return the newly created issue', async () => {
+      const createIssueDto = {
+        title: 'Test Issue',
+        description: 'This is a test issue',
+      };
+      const result = await controller.addIssue(
+        {
+          user: { sub: user.id },
+        },
+        org.id,
+        createIssueDto,
+      );
+      expect(result.title).toBe(createIssueDto.title);
+      expect(result.description).toBe(createIssueDto.description);
+    });
+  });
+
+  describe('when listing issues', () => {
+    it('should return a list of issues', async () => {
+      const createIssueDto = {
+        title: 'Test Issue',
+        description: 'This is a test issue',
+      };
+      await controller.addIssue(
+        {
+          user: { sub: user.id },
+        },
+        org.id,
+        createIssueDto,
+      );
+      const result = await controller.listIssues(org.id);
+      expect(result.length).toBe(1);
+      expect(result[0].title).toBe(createIssueDto.title);
+      expect(result[0].description).toBe(createIssueDto.description);
+    });
+  });
+
+  describe('when getting an issue by id', () => {
+    it('should return the issue', async () => {
+      const createIssueDto = {
+        title: 'Test Issue',
+        description: 'This is a test issue',
+      };
+      const { id } = await controller.addIssue(
+        {
+          user: { sub: user.id },
+        },
+        org.id,
+        createIssueDto,
+      );
+      const result = await controller.getIssueById(org.id, id);
+      expect(result.title).toBe(createIssueDto.title);
+      expect(result.description).toBe(createIssueDto.description);
+    });
+
+    it('should throw an error if the org does not exist', async () => {
+      await expect(
+        controller.getIssueById(org.id, 'non-existent-id'),
+      ).rejects.toThrow();
+    });
+  });
+
+  describe('when updating an issue', () => {
+    it('should return the updated issue', async () => {
+      const createIssueDto = {
+        title: 'Test Issue',
+        description: 'This is a test issue',
+      };
+      const { id } = await controller.addIssue(
+        {
+          user: { sub: user.id },
+        },
+        org.id,
+        createIssueDto,
+      );
+      const updateIssueDto = {
+        title: 'Updated Issue',
+        description: 'This is an updated issue',
+      };
+      const result = await controller.updateIssue(
+        {
+          user: { sub: user.id },
+        },
+        org.id,
+        id,
+        updateIssueDto,
+      );
+      expect(result.title).toBe(updateIssueDto.title);
+      expect(result.description).toBe(updateIssueDto.description);
+    });
+  });
+
+  describe('when deleting an issue', () => {
+    it('should delete the issue', async () => {
+      const createIssueDto = {
+        title: 'Test Issue',
+        description: 'This is a test issue',
+      };
+      const { id } = await controller.addIssue(
+        {
+          user: { sub: user.id },
+        },
+        org.id,
+        createIssueDto,
+      );
+      await controller.deleteIssue(
+        {
+          user: { sub: user.id },
+        },
+        org.id,
+        id,
+      );
+      await expect(controller.getIssueById(org.id, id)).rejects.toThrow();
+    });
   });
 });
--- src/issues/issues.controller.ts
+++ src/issues/issues.controller.ts
@@ -1,4 +1,112 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { IssuesService } from './issues.service';
+import { AuthGuard } from '../auth/auth.guard';
+import { IssueDto } from './dtos';
+import { Public } from '../auth/public.guard';
 
-@Controller('issues')
-export class IssuesController {}
+@Controller('/orgs/:orgId/issues')
+export class IssuesController {
+  constructor(private issuesService: IssuesService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @UseGuards(AuthGuard)
+  async addIssue(
+    @Request() request,
+    @Param('orgId') orgId: string,
+    @Body() issueDto: IssueDto,
+  ) {
+    try {
+      return await this.issuesService.addIssue(
+        request.user.sub,
+        orgId,
+        issueDto,
+      );
+    } catch (e) {
+      throw new BadRequestException(e.message);
+    }
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  @Public()
+  async listIssues(
+    @Param('orgId') orgId: string,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 0,
+  ) {
+    try {
+      return await this.issuesService.listIssues(orgId, page, limit);
+    } catch (e) {
+      throw new BadRequestException(e.message);
+    }
+  }
+
+  @Get(':issueId')
+  @HttpCode(HttpStatus.OK)
+  @Public()
+  async getIssueById(
+    @Param('orgId') orgId: string,
+    @Param('issueId') issueId: string,
+  ) {
+    try {
+      return await this.issuesService.getIssueById(orgId, issueId);
+    } catch (e) {
+      throw new BadRequestException(e.message);
+    }
+  }
+
+  @Put(':issueId')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard)
+  async updateIssue(
+    @Request() request,
+    @Param('orgId') orgId: string,
+    @Param('issueId') issueId: string,
+    @Body() issueDto: IssueDto,
+  ) {
+    try {
+      return await this.issuesService.updateIssue(
+        request.user.sub,
+        orgId,
+        issueId,
+        issueDto,
+      );
+    } catch (e) {
+      throw new BadRequestException(e.message);
+    }
+  }
+
+  @Delete(':issueId')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard)
+  async deleteIssue(
+    @Request() request,
+    @Param('orgId') orgId: string,
+    @Param('issueId') issueId: string,
+  ) {
+    try {
+      return await this.issuesService.deleteIssue(
+        request.user.sub,
+        orgId,
+        issueId,
+      );
+    } catch (e) {
+      throw new BadRequestException(e.message);
+    }
+  }
+}
