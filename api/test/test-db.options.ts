1237dbfb8acee56f6fe9712cd0337586274da50e
Daniel Chivu <chivu.9019@gmail.com>
2024-11-12 11:22:24 +0200

    Modify workflows to accomodate the migration

--- api/test/test-db.options.ts
+++ api/test/test-db.options.ts
@@ -1,12 +1,14 @@
 import { DataSourceOptions } from 'typeorm';
+import * as process from 'node:process';
 
 export const testDbOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5455,
-  username: 'testuser',
-  password: 'testpass',
-  database: 'testdb',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  ssl: false,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../src/migrations/*{.ts,.js}'],
   migrationsRun: true,
