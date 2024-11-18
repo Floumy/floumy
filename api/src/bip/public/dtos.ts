a3c609db8017971e4a2ecc334a9aa84e21ff983a
Alexandru Nedelcu <lexnede@gmail.com>
2024-11-18 21:44:51 +0100

    Remove unnecessary files

--- api/src/bip/public/dtos.ts
+++
@@ -1,7 +0,0 @@
-export interface PublicSettingsDto {
-  isBuildInPublicEnabled: boolean;
-  isObjectivesPagePublic: boolean;
-  isRoadmapPagePublic: boolean;
-  isIterationsPagePublic: boolean;
-  isActiveIterationsPagePublic: boolean;
-}
