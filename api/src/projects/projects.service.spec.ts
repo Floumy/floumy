820df4be31dd218354ffe4e761c79674a8cb8bfd
Alexandru Nedelcu <lexnede@gmail.com>
2024-11-23 22:53:36 +0100

    WI-416: Add list projects service method

--- api/src/projects/projects.service.spec.ts
+++ api/src/projects/projects.service.spec.ts
@@ -1,18 +1,60 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectsService } from './projects.service';
+import { User } from '../users/user.entity';
+import { Org } from '../orgs/org.entity';
+import { Project } from './project.entity';
+import { setupTestingModule } from '../../test/test.utils';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from '../users/users.service';
+import { Repository } from 'typeorm';
 
 describe('ProjectsService', () => {
   let service: ProjectsService;
+  let user: User;
+  let org: Org;
+  let projectsRepository: Repository<Project>;
+  let cleanup: () => Promise<void>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectsService],
-    }).compile();
-
+    const { module, cleanup: dbCleanup } = await setupTestingModule(
+      [TypeOrmModule.forFeature([Project, User])],
+      [ProjectsService, UsersService],
+    );
     service = module.get<ProjectsService>(ProjectsService);
+    const usersService = module.get<UsersService>(UsersService);
+    projectsRepository = module.get<Repository<Project>>(
+      getRepositoryToken(Project),
+    );
+    user = await usersService.createUserWithOrg(
+      'Test User',
+      'test@example.com',
+      'testtesttest',
+    );
+    org = await user.org;
+    cleanup = dbCleanup;
+  });
+
+  afterEach(async () => {
+    await cleanup();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('when listing projects', () => {
+    it('should return the list of projects', async () => {
+      const project = new Project();
+      project.name = 'Test Project';
+      project.org = Promise.resolve(org);
+      user.projects = Promise.resolve([project]);
+      await projectsRepository.save(project);
+      const projects = await service.listProjects(org.id);
+      expect(projects).toBeDefined();
+      expect(projects.length).toEqual(2);
+      expect(projects[0].id).toBeDefined();
+      expect(projects[0].name).toEqual('Test Project');
+      expect(projects[1].id).toBeDefined();
+      expect(projects[1].name).toEqual('Default Project');
+    });
   });
 });
--- api/src/projects/projects.service.ts
+++ api/src/projects/projects.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from './project.entity';
+import { Repository } from 'typeorm';
+import { ProjectMapper } from './mappers';
 
 @Injectable()
 export class ProjectsService {
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectsRepository: Repository<Project>,
+  ) {}
+
   async listProjects(orgId: string) {
-    return Promise.resolve(undefined);
+    const projects = await this.projectsRepository.find({
+      where: { org: { id: orgId } },
+      order: { createdAt: 'DESC' },
+    });
+    return await Promise.all(projects.map(ProjectMapper.toDto));
   }
 }
