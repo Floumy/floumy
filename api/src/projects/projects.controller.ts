d6468c482ef8baf5cbc4fb81909131c9d04556c3
Alexandru Nedelcu <lexnede@gmail.com>
2024-11-24 21:33:45 +0100

    WI-416: Add Project Controller project listing test

--- api/src/projects/projects.controller.ts
+++ api/src/projects/projects.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, Request, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('orgs/:orgId/projects')
 export class ProjectsController {
-  constructor(private projectsService: ProjectsService) {}
+  constructor(private projectsService: ProjectsService) {
+  }
 
   @UseGuards(AuthGuard)
   @Get()
-  async listProjects(@Param('orgId') orgId: string) {
-    return await this.projectsService.listProjects(orgId);
+  async listProjects(@Request() request, @Param('orgId') orgId: string) {
+    if (orgId !== request.user.org) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      return await this.projectsService.listProjects(orgId);
+    } catch (e) {
+      throw new BadRequestException(e.message);
+    }
   }
 }
