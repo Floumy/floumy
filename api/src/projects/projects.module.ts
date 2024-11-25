bec60a7dcb0ad82d96a816ad5321b34fd2e2fe9b
Alexandru Nedelcu <lexnede@gmail.com>
2024-11-25 21:17:03 +0100

    WI-416: Fix projects module

--- api/src/projects/projects.module.ts
+++ api/src/projects/projects.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
+import { Project } from './project.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from '../users/users.module';
+import { AuthModule } from '../auth/auth.module';
+import { User } from '../users/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Project, User]), UsersModule, AuthModule],
   providers: [ProjectsService],
   controllers: [ProjectsController],
 })
