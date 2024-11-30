3294226a6f1fa8cf98f34837ef8163d8abbb0415
Alexandru Nedelcu <lexnede@gmail.com>
2024-11-30 21:19:45 +0100

    WI-416: Project update and delete

--- web/src/services/projects/projects.service.js
+++ web/src/services/projects/projects.service.js
@@ -17,3 +17,20 @@ export async function createProject(orgId, projectName) {
     throw new Error(e.message);
   }
 }
+
+export async function updateProject(orgId, projectId, projectName) {
+  try {
+    const response = await api.put(`${process.env.REACT_APP_API_URL}/orgs/${orgId}/my-projects/${projectId}`, { name: projectName });
+    return response.data;
+  } catch (e) {
+    throw new Error(e.message);
+  }
+}
+
+export async function deleteProject(orgId, projectId) {
+  try {
+    await api.delete(`${process.env.REACT_APP_API_URL}/orgs/${orgId}/my-projects/${projectId}`);
+  } catch (e) {
+    throw new Error(e.message);
+  }
+}
