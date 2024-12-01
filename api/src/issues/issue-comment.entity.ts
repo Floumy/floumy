b7a4e02d59e9534375294a0763accc0debf3b15f
Daniel Chivu <chivu.9019@gmail.com>
2024-12-01 10:12:18 +0200

    Integrate mentions in issues

--- api/src/issues/issue-comment.entity.ts
+++ api/src/issues/issue-comment.entity.ts
@@ -1,9 +1,26 @@
-import { Entity, ManyToOne } from 'typeorm';
+import { Entity, JoinTable, ManyToMany, ManyToOne } from 'typeorm';
 import { Issue } from './issue.entity';
 import { Comment } from '../comments/comment-entity';
+import { User } from '../users/user.entity';
 
 @Entity()
 export class IssueComment extends Comment {
   @ManyToOne(() => Issue, (issue) => issue.comments, { lazy: true })
   issue: Promise<Issue>;
+
+  @ManyToMany(() => User, {
+    lazy: true,
+  })
+  @JoinTable({
+    name: 'issue_comment_mentions',
+    joinColumn: {
+      name: 'issueCommentId',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'userId',
+      referencedColumnName: 'id',
+    },
+  })
+  mentions: User[];
 }
