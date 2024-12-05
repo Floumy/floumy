eef438a13bb9044e086d7f52661837b1c675e78a
Alexandru Nedelcu <lexnede@gmail.com>
2024-12-05 22:01:50 +0100

    WI-448: Fix public sidebar + fix public checks

--- api/src/projects/projects.module.ts
+++ api/src/projects/projects.module.ts
@@ -7,14 +7,18 @@ import { UsersModule } from '../users/users.module';
 import { AuthModule } from '../auth/auth.module';
 import { User } from '../users/user.entity';
 import { Org } from '../orgs/org.entity';
+import { PublicController } from './public/public.controller';
+import { CacheModule } from '@nestjs/cache-manager';
+import { PublicService } from './public/public.service';
 
 @Module({
   imports: [
+    CacheModule.register(),
     TypeOrmModule.forFeature([Project, User, Org]),
     UsersModule,
     AuthModule,
   ],
-  providers: [ProjectsService],
-  controllers: [ProjectsController],
+  providers: [ProjectsService, PublicService],
+  controllers: [ProjectsController, PublicController],
 })
 export class ProjectsModule {}
