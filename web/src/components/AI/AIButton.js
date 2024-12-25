eedf88d44a9eca22ccb790e889ee596e607a370f
Alexandru Nedelcu <lexnede@gmail.com>
2024-12-25 21:57:42 +0200

    Add initiatives to OKRs using AI

---
+++ web/src/components/AI/AIButton.js
@@ -0,0 +1,30 @@
+import { Button } from 'reactstrap';
+import { useState } from 'react';
+
+const AIButton = ({ onClick, text = 'Fill with AI', disabled = false }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  return (
+    <Button
+      color="link"
+      size="sm"
+      className="ml-2 p-0 shadow-none"
+      style={{ color: '#9b59b6' }}
+      disabled={disabled}
+      onClick={async () => {
+        setIsLoading(true);
+        await onClick();
+        setIsLoading(false);
+      }}
+    >
+      {isLoading ? (
+        <i className="fas fa-spinner fa-spin fa-1x"></i>
+      ) : (
+        <>
+          <i className="fas fa-wand-magic-sparkles"></i> {text}
+        </>
+      )}
+    </Button>
+  );
+};
+
+export default AIButton;
