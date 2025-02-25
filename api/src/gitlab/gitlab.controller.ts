9d071fbcb65c8f436a485a049bfb6da763277ed0
Alexandru Nedelcu <lexnede@gmail.com>
2025-02-25 21:49:01 +0100

    Add Gitlab token to org

--- api/src/gitlab/gitlab.controller.ts
+++ api/src/gitlab/gitlab.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { GitlabService } from './gitlab.service';
 
 @Controller('gitlab')
-export class GitlabController {}
+export class GitlabController {
+  constructor(private readonly gitlabService: GitlabService) {}
+
+  @Post('/auth/orgs/:orgId/token')
+  async setToken(@Param('orgId') orgId: string, @Body('token') token: string) {
+    await this.gitlabService.setToken(orgId, token);
+  }
+}
