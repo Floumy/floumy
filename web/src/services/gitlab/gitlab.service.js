d1ef22a0b41d05db7c14e87789dca6d50a2423e2
Alexandru Nedelcu <lexnede@gmail.com>
2025-03-22 21:05:39 +0100

    Add frontend service for gitlab integration

---
+++ web/src/services/gitlab/gitlab.service.js
@@ -0,0 +1,28 @@
+import api from '../api/api.service';
+
+export async function listProjects(orgId) {
+  try {
+    const response = await api.get(`${process.env.REACT_APP_API_URL}/gitlab/orgs/${orgId}/projects`);
+    return response.data;
+  } catch (e) {
+    throw new Error(e.message);
+  }
+}
+
+export async function setProject(orgId, projectId, gitlabProjectId) {
+  try {
+    const response = await api.put(`${process.env.REACT_APP_API_URL}/gitlab/orgs/${orgId}/projects/${projectId}`, { gitlabProjectId });
+    return response.data;
+  } catch (e) {
+    throw new Error(e.message);
+  }
+}
+
+export async function setToken(orgId, token) {
+  try {
+    const response = await api.put(`${process.env.REACT_APP_API_URL}/gitlab/auth/orgs/${orgId}/token`, { token });
+    return response.data;
+  } catch (e) {
+    throw new Error(e.message);
+  }
+}
