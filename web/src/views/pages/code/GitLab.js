1efbcd8a938c0c7767c7360cec3a67a5aace5bb7
Alexandru Nedelcu <lexnede@gmail.com>
2025-03-26 22:16:59 +0100

    Use Gitlab / Github components on the Code page

--- web/src/views/pages/code/GitLab.js
+++ web/src/views/pages/code/GitLab.js
@@ -1,43 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import InfiniteLoadingBar from '../components/InfiniteLoadingBar';
+import React from 'react';
 import SimpleHeader from '../../../components/Headers/SimpleHeader';
-import { Container } from 'reactstrap';
-import { useProjects } from '../../../contexts/ProjectsContext';
-import { toast } from 'react-toastify';
-import { getIsGitLabConnected } from '../../../services/gitlab/gitlab.service';
+import { Card, CardBody, CardHeader, CardTitle, Col, Container, Row, UncontrolledTooltip } from 'reactstrap';
+import InfiniteLoadingBar from '../components/InfiniteLoadingBar';
+import UpdateWarning from '../components/UpdateWarning';
 
-function GitLab() {
-  const { orgId, currentProject } = useProjects();
+function GitLab({repo}) {
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [isGitLabConnected, setIsGitLabConnected] = useState(true);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [disconnectWarning, setDisconnectWarning] = React.useState(false);
 
-  useEffect(() => {
-    if (!currentProject?.id || !orgId) return;
+  const handleRepoDisconnect = async () => {}
+  const editRepo = async () => {}
 
-    const fetchGitLabData = async () => {
-      setIsLoading(true);
-      try {
-        const { connected, repo } = await getIsGitLabConnected(orgId, currentProject.id);
-        setIsGitLabConnected(connected);
-      } catch (error) {
-        setIsGitLabConnected(false);
-        toast.error('Failed to check GitLab connection');
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchGitLabData();
-  }, [currentProject?.id, orgId]);
   return (
     <>
       {isLoading && <InfiniteLoadingBar />}
       <SimpleHeader />
       <Container className="mt--6" fluid id="OKRs">
-        {isGitLabConnected && <>
-          GiLab is connected
-        </>}
+        <UpdateWarning
+          isOpen={disconnectWarning}
+          toggle={() => setDisconnectWarning(!disconnectWarning)}
+          warningMessage={"Are you sure you want to disconnect from GitHub?"}
+          entity={"connection to GitHub"}
+          onUpdate={async () => await handleRepoDisconnect()} />
+        <Row>
+          <Col>
+            <Card className="mb-5">
+              <CardHeader>
+                <Row>
+                  <Col md={12}>
+                    <CardTitle tag="h2" className="mb-3"> {repo ? <>
+                      Pull Requests {" "}
+                      <a className="btn-link text-blue mr-2" href={repo.url} target="_blank" rel="noreferrer">
+                        | {repo.name}
+                      </a>
+                      <i className="fa fa-edit mr-2" style={{ cursor: 'pointer' }} onClick={editRepo} />
+                      <UncontrolledTooltip target="disconnect-from-github" placement="top">
+                        Disconnect from GitHub
+                      </UncontrolledTooltip>
+                      <i className="fa fa-xmark mr-2" style={{ cursor: 'pointer' }} id="disconnect-from-github" onClick={() => setDisconnectWarning(true)} />
+                    </> : <span className="mr-2">Code</span>}
+                    </CardTitle>
+                  </Col>
+                </Row>
+              </CardHeader>
+              <CardBody>
+
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </>
   );
