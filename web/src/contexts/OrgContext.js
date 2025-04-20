7456e0ab7ea135d4941f9b55f2be413152575b1e
Alexandru Nedelcu <lexnede@gmail.com>
2025-04-20 15:20:47 +0200

    Add organization context and enhance sidebar functionality
    Introduce `OrgContext` to manage organization-specific data. Update layouts and sidebars to fetch and display organization details dynamically. Simplify sidebar navigation and improve project listing structure for better usability.

---
+++ web/src/contexts/OrgContext.js
@@ -0,0 +1,49 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { getOrg } from '../services/org/orgs.service';
+
+const OrgContext = createContext({});
+
+export function OrgProvider({ children, orgId }) {
+  const [currentOrg, setCurrentOrg] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchOrg = async () => {
+      try {
+        setLoading(true);
+        const currentOrg = await getOrg();
+        if (currentOrg) {
+          setCurrentOrg(currentOrg);
+        }
+      } catch (error) {
+        console.error('Error fetching org:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (orgId) {
+      fetchOrg();
+    }
+  }, [orgId]);
+
+  const value = {
+    orgId,
+    currentOrg,
+    loading,
+  };
+
+  return (
+    <OrgContext.Provider value={value}>
+      {children}
+    </OrgContext.Provider>
+  );
+}
+
+export function useOrg() {
+  const context = useContext(OrgContext);
+  if (context === undefined) {
+    throw new Error('useOrg must be used within an OrgProvider');
+  }
+  return context;
+}
