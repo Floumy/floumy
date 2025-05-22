eb4e78b3c1faf3fb15c0312d13a9a4360623ffeb
Alexandru Nedelcu <lexnede@gmail.com>
2025-05-22 22:21:22 +0200

    Add user lookup when assigning initiatives
    Updated initiative assignment logic to fetch user details via `getUser` instead of directly using the user ID. Added a new endpoint in the API to retrieve user details and integrated its service in both backend and frontend. Also updated tests to cover these changes.

--- web/src/views/pages/roadmap/InitiativesRoadmap.js
+++ web/src/views/pages/roadmap/InitiativesRoadmap.js
@@ -15,6 +15,7 @@ import LoadingSpinnerBox from '../components/LoadingSpinnerBox';
 import { useHotkeys } from 'react-hotkeys-hook';
 import { sortByPriority } from '../../../services/utils/utils';
 import InitiativesListCard from '../initiatives/InitiativesListCard';
+import { getUser } from '../../../services/okrs/okrs.service';
 
 function InitiativesRoadmap() {
   const { orgId, projectId } = useParams();
@@ -121,11 +122,12 @@ function InitiativesRoadmap() {
     setInitiatives([...sortByPriority(updatedInitiativesList)]);
   }
 
-  function updateBacklogInitiativesAssignedTo(updatedInitiatives, newAssignedTo) {
+  async function updateBacklogInitiativesAssignedTo(updatedInitiatives, newAssignedTo) {
+    const user = await getUser(orgId, newAssignedTo);
     const updatedInitiativesIds = updatedInitiatives.map(f => f.id);
     const updatedInitiativesList = initiatives.map(initiative => {
       if (updatedInitiativesIds.includes(initiative.id)) {
-        initiative.assignedTo = newAssignedTo;
+        initiative.assignedTo = user;
       }
       return initiative;
     });
