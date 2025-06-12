8e6ed24c6227d08b9f7f4562f85415f79db03a5e
Alexandru Nedelcu <lexnede@gmail.com>
2025-06-12 21:32:46 +0200

    Add role field to User entity
    Introduce a "role" column to the "user" table with a default value of "contributor" and an enum type for roles. This enhances role-based functionality by defining user roles as "admin" or "contributor" in the application.

---
+++ api/src/migrations/1749755212947-addRoleToUser.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddRoleToUser1749755212947 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD COLUMN "role" varchar NOT NULL DEFAULT 'contributor'`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "role"`);
+  }
+}
