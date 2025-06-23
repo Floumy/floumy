70b45e1f057a9bd71cc61f3a057018230d0208d7
Alexandru Nedelcu <lexnede@gmail.com>
2025-06-23 20:59:35 +0200

    Refactor role-based access control
    - Updated `Roles` decorator to use `UserRole` enum for stricter type safety.
    - Modified `BasicAuthGuard` to handle detailed user role information.
    - Adjusted role-based guards in `UsersController` and `OrgsController` to use `UserRole.ADMIN`.
    - Enhanced user role migration by introducing `user_role_enum` for better role management.

--- api/src/migrations/1749755212947-addRoleToUser.ts
+++ api/src/migrations/1749755212947-addRoleToUser.ts
@@ -3,11 +3,15 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AddRoleToUser1749755212947 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "user" ADD COLUMN "role" varchar NOT NULL DEFAULT 'admin'`,
+      `CREATE TYPE "user_role_enum" AS ENUM('admin', 'contributor')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD COLUMN "role" "user_role_enum" NOT NULL DEFAULT 'admin'`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "role"`);
+    await queryRunner.query(`DROP TYPE "user_role_enum"`);
   }
 }
