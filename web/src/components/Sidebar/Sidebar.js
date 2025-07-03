ed846096811d9643a9619b0bf12322f1d24b9a51
Daniel Chivu <chivu.9019@gmail.com>
2025-07-03 23:02:16 +0300

    Enable Pages

--- web/src/components/Sidebar/Sidebar.js
+++ web/src/components/Sidebar/Sidebar.js
@@ -520,25 +520,23 @@ function Sidebar({ toggleSidenav, logo, rtlActive }) {
                     </Col>
                   </Row>
                 </NavItem>
-                {isFeatureEnabled('PAGES') && (
-                  <NavItem>
-                    <Row style={{ maxWidth: '100%', height: '47px' }}>
-                      <Col xs={10}>
-                        <NavLink
-                          to={`/admin/orgs/${orgId}/projects/${currentProject.id}/pages`}
-                          onClick={closeSidenav}
-                          tag={NavLinkRRD}
-                        >
-                          <i className="fa fa-file" />
-                          <span className="nav-link-text">Pages</span>
-                        </NavLink>
-                      </Col>
-                      <Col xs={2} className="text-right pr-2 pt-2">
-                        <ShortcutIcon shortcutKey={9} />
-                      </Col>
-                    </Row>
-                  </NavItem>
-                )}
+                <NavItem>
+                  <Row style={{ maxWidth: '100%', height: '47px' }}>
+                    <Col xs={10}>
+                      <NavLink
+                        to={`/admin/orgs/${orgId}/projects/${currentProject.id}/pages`}
+                        onClick={closeSidenav}
+                        tag={NavLinkRRD}
+                      >
+                        <i className="fa fa-file" />
+                        <span className="nav-link-text">Pages</span>
+                      </NavLink>
+                    </Col>
+                    <Col xs={2} className="text-right pr-2 pt-2">
+                      <ShortcutIcon shortcutKey={9} />
+                    </Col>
+                  </Row>
+                </NavItem>
               </Nav>
             </Collapse>
           </div>
