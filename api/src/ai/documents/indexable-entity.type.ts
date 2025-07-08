a45d906a3e132ce46079761405a2bdd2161b11a4
Alexandru Nedelcu <lexnede@gmail.com>
2025-07-08 20:41:13 +0200

    Remove unused `IndexableEntityType` enum from AI documents module

--- api/src/ai/documents/indexable-entity.type.ts
+++
@@ -1,9 +0,0 @@
-export enum IndexableEntityType {
-  OBJECTIVE = 'objective',
-  INITIATIVE = 'initiative',
-  WORK_ITEM = 'work_item',
-  MILESTONE = 'milestone',
-  SPRINT = 'sprint',
-  FEATURE_REQUEST = 'feature_request',
-  ISSUE = 'issue',
-}
