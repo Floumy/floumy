da65846a857764ac3e52cb72e9e88a701760fa0b
Alexandru Nedelcu <lexnede@gmail.com>
2025-07-11 22:50:28 +0200

    Extract mcp functionality to a separate module + add first try at a real tool for work items

--- api/src/mcp/mcp.module.ts
+++ api/src/mcp/mcp.module.ts
@@ -1,42 +1,42 @@
-import { Module } from "@nestjs/common";
-import { GreetingTool } from "./tools/greeting.tool";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { User } from "src/users/user.entity";
-import { Org } from "src/orgs/org.entity";
-import { Project } from "src/projects/project.entity";
-import { WorkItem } from "src/backlog/work-items/work-item.entity";
-import { Initiative } from "src/roadmap/initiatives/initiative.entity";
-import { Sprint } from "src/sprints/sprint.entity";
-import { Issue } from "src/issues/issue.entity";
-import { FeatureRequest } from "src/feature-requests/feature-request.entity";
-import { KeyResult } from "src/okrs/key-result.entity";
-import { Objective } from "src/okrs/objective.entity";
-import { Milestone } from "src/roadmap/milestones/milestone.entity";
-import { McpModule } from "@rekog/mcp-nest";
-import { WorkItemsTool } from "./tools/work-items.tool";
+import { Module } from '@nestjs/common';
+import { GreetingTool } from './tools/greeting.tool';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
+import { Org } from 'src/orgs/org.entity';
+import { Project } from 'src/projects/project.entity';
+import { WorkItem } from 'src/backlog/work-items/work-item.entity';
+import { Initiative } from 'src/roadmap/initiatives/initiative.entity';
+import { Sprint } from 'src/sprints/sprint.entity';
+import { Issue } from 'src/issues/issue.entity';
+import { FeatureRequest } from 'src/feature-requests/feature-request.entity';
+import { KeyResult } from 'src/okrs/key-result.entity';
+import { Objective } from 'src/okrs/objective.entity';
+import { Milestone } from 'src/roadmap/milestones/milestone.entity';
+import { McpModule } from '@rekog/mcp-nest';
+import { WorkItemsTool } from './tools/work-items.tool';
 
 @Module({
-    providers: [GreetingTool, WorkItemsTool],
-    imports: [
-        McpModule.forRoot({
-            name: 'floumy-mcp-server',
-            version: '1.0.0',
-            sseEndpoint: '/mcp/:orgId/:userId/sse',
-            messagesEndpoint: '/mcp/:orgId/:userId/messages',
-        }),
-        TypeOrmModule.forFeature([
-            User,
-            Org,
-            Project,
-            WorkItem,
-            Initiative,
-            Sprint,
-            Issue,
-            KeyResult,
-            Objective,
-            FeatureRequest,
-            Milestone,
-        ]),
-    ],
+  providers: [GreetingTool, WorkItemsTool],
+  imports: [
+    McpModule.forRoot({
+      name: 'floumy-mcp-server',
+      version: '1.0.0',
+      sseEndpoint: '/mcp/:orgId/:userId/sse',
+      messagesEndpoint: '/mcp/:orgId/:userId/messages',
+    }),
+    TypeOrmModule.forFeature([
+      User,
+      Org,
+      Project,
+      WorkItem,
+      Initiative,
+      Sprint,
+      Issue,
+      KeyResult,
+      Objective,
+      FeatureRequest,
+      Milestone,
+    ]),
+  ],
 })
-export class FloumyMcpModule { }
+export class FloumyMcpModule {}
