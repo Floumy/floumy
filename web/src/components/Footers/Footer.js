3fd8f7916714d4b5b02449cce1047c6017f48936
Alexandru Nedelcu <lexnede@gmail.com>
2025-07-18 20:16:46 +0200

    Change the terms of service and privacy policy links

--- web/src/components/Footers/Footer.js
+++ web/src/components/Footers/Footer.js
@@ -22,27 +22,13 @@ function Footer({ justifyContent = 'left' }) {
                 </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink
-                  href="https://app.termly.io/policy-viewer/policy.html?policyUUID=fb8deed6-e77a-43cd-aa76-1c655b357e4c"
-                  target="_blank"
-                >
+                <NavLink href="https://floumy.com/privacy" target="_blank">
                   Privacy Policy
                 </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink
-                  href="https://app.termly.io/policy-viewer/policy.html?policyUUID=b76fc02b-bf3a-4da0-a77b-dcb50b8d37c2"
-                  target="_blank"
-                >
+                <NavLink href="https://floumy.com/terms" target="_blank">
                   Terms of Service
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  href="https://app.termly.io/policy-viewer/policy.html?policyUUID=55201b2f-f6a3-4311-9d9e-1de781d15f55"
-                  target="_blank"
-                >
-                  Cookie Policy
                 </NavLink>
               </NavItem>
             </Nav>
