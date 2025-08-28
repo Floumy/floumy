fc03b841dc7a2ad6682fd2ce9e7b74915991151f
Alexandru Nedelcu <lexnede@gmail.com>
2025-08-28 18:32:53 +0200

    Remove unused `ShortcutKey` styled component from `ShortcutIcon` to clean up unused code.

--- web/src/components/Shortcuts/ShortcutIcon.js
+++ web/src/components/Shortcuts/ShortcutIcon.js
@@ -16,19 +16,6 @@ const ItemName = styled.span`
   color: #ffffff;
 `;
 
-const ShortcutKey = styled.span`
-  background-color: #1e232b;
-  color: #e1e1e1;
-  font-weight: bold;
-  padding: 4px;
-  text-align: center;
-  margin: 2px 0;
-  border-radius: 4px;
-  font-size: 12px;
-  width: 27px;
-  height: 27px;
-`;
-
 const ShortcutIcon = ({ itemName, shortcutKey }) => (
   <ShortcutContainer className="py-1">
     <ItemName>{itemName}</ItemName>
