bd7f67d89a34cb0d37858dd40a2b39bab0ff3109
Daniel Chivu <chivu.9019@gmail.com>
2025-09-20 23:26:20 +0300

    Fix api url in dev

--- web/src/config.js
+++ web/src/config.js
@@ -1,1 +1,4 @@
-export const apiUrl = window.REACT_APP_API_URL || 'http://localhost:8080';
+export const apiUrl =
+  process.env.NODE_ENV === 'production'
+    ? window.REACT_APP_API_URL
+    : process.env.REACT_APP_API_URL;
